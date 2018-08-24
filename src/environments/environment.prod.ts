0a42540b7c2922a178d3edf4f6ca5b55aaf274ae
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-24 00:18:08 -0500

    Add firebase info.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: 'tHrFVTl3ni2b7BgOoVJiZ2gFUZwNhOngzbxS8kSb',
+      authDomain: 'sensor-monitor-5e35f.firebaseapp.com',
+      databaseURL: 'https://sensor-monitor-5e35f.firebaseio.com',
+      projectId: 'sensor-monitor-5e35f',
+      storageBucket: 'sensor-monitor-5e35f.appspot.com',
+      messagingSenderId: 'ZZZZZZZZZZZZ'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: 'tHrFVTl3ni2b7BgOoVJiZ2gFUZwNhOngzbxS8kSb',
+      authDomain: 'sensor-monitor-5e35f.firebaseapp.com',
+      databaseURL: 'https://sensor-monitor-5e35f.firebaseio.com',
+      projectId: 'sensor-monitor-5e35f',
+      storageBucket: 'sensor-monitor-5e35f.appspot.com',
+      messagingSenderId: 'ZZZZZZZZZZZZ'
+  }
 };
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
