682c2af1fe0d08f2e9ec288a186ef452ee311106
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-24 02:01:48 -0500

    Add functionality to sidenav.

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
-  styleUrls: ['./sidenav-list.component.css']
+  styleUrls: ['./sidenav-list.component.scss']
 })
 export class SidenavListComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClose() {
+    this.closeSidenav.emit();
+  }
 }
