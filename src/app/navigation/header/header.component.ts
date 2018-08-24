8f25f1546fa200259bc90120cac0271377531695
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-24 02:02:05 -0500

    Add functionality to header.

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isEmployee: Observable<Boolean>;
+  displayName: string;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
 }
