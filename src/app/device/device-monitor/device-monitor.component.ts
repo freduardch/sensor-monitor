61881d0a378b33af66da3c81be860d56f26d9e4c
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-26 14:37:03 -0500

    Create device monitor component.

---
+++ src/app/device/device-monitor/device-monitor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-monitor',
+  templateUrl: './device-monitor.component.html',
+  styleUrls: ['./device-monitor.component.scss']
+})
+export class DeviceMonitorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
