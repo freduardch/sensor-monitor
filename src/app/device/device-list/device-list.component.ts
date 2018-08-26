fc220e0ff474c0e2ebebfec7cf368eed9fac9b5c
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-26 14:28:53 -0500

    Obtain info from database.

--- src/app/device/device-list/device-list.component.ts
+++ src/app/device/device-list/device-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { DeviceService } from '../../device.service';
+import { Subscription } from 'rxjs';
+import { Device } from '../../device.model';
 
 @Component({
   selector: 'app-device-list',
   templateUrl: './device-list.component.html',
   styleUrls: ['./device-list.component.scss']
 })
-export class DeviceListComponent implements OnInit {
+export class DeviceListComponent implements OnInit, OnDestroy {
+  dataSource = new MatTableDataSource();
 
-  constructor() { }
+  displayedColumns = ['name', 'location', 'status', 'temperature', 'humidity'];
+  subscriptions: Subscription[] = [];
+
+  constructor(private deviceService: DeviceService) { }
 
   ngOnInit() {
+    this.subscriptions.push(this.deviceService.getAllDevices()
+      .subscribe((devices: Device[]) => {
+        this.dataSource = new MatTableDataSource(devices);
+        console.log(this.dataSource);
+      }));
+  }
+
+  ngOnDestroy() {
+    if (this.subscriptions.length > 0) {
+      this.subscriptions.forEach((subscription) => {
+        subscription.unsubscribe();
+      });
+    }
   }
 
 }
