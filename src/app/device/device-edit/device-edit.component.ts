412beab0a437388b8601b55ab8e973d6e71f97f1
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-26 14:29:16 -0500

    Add editing functionality.

--- src/app/device/device-edit/device-edit.component.ts
+++ src/app/device/device-edit/device-edit.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Device } from '../../device.model';
+import { Subscription } from 'rxjs';
+import { DeviceService } from '../../device.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-device-edit',
   templateUrl: './device-edit.component.html',
   styleUrls: ['./device-edit.component.scss']
 })
-export class DeviceEditComponent implements OnInit {
+export class DeviceEditComponent implements OnInit, OnDestroy {
+  currentDevice: Device;
+  currentDeviceId: string;
 
-  constructor() { }
+  subscriptions: Subscription[] = [];
+
+  constructor(private deviceService: DeviceService,
+    private activatedRoute: ActivatedRoute) {
+    this.subscriptions.push(
+    this.activatedRoute.params.subscribe((param) => {
+    this.currentDeviceId = param['id'];
+      })
+    );
+               }
 
   ngOnInit() {
+    this.subscriptions.push(this.deviceService.getDeviceById(this.currentDeviceId)
+    .subscribe((device: Device) => {
+      this.currentDevice = device;
+    }));
+  }
+
+  ngOnDestroy() {
+  }
+
+  onEditDevice() {
+
   }
 
 }
