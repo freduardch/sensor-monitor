bcf4106f8c3ed7187f8dd04c32d551e39dd0367f
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-26 11:41:09 -0500

    Add more device methods.

--- src/app/device.service.ts
+++ src/app/device.service.ts
@@ -24,6 +24,26 @@ export class DeviceService {
 
   /**
    *
+   * @param device
+   */
+  updateDevice(id: string, device: Device): string {
+    this.database.doc<Device>(`devices/${id}`).update({...device});
+
+    return id;
+  }
+
+  /**
+   *
+   * @param device
+   */
+  deleteDevice(id: string): string {
+    this.database.doc<Device>(`devices/${id}`).delete();
+
+    return id;
+  }
+
+  /**
+   *
    * @param id
    */
   getDeviceById(id: string): Observable<Device> {
@@ -32,8 +52,59 @@ export class DeviceService {
     .pipe(
       map((document) => {
         return {
-          info: document.payload.data().info,
+          id: document.payload.id,
+          name: document.payload.data().name,
+          location: document.payload.data().location,
+          status: document.payload.data().status,
+          temperature: document.payload.data().temperature,
+          humidity: document.payload.data().humidity,
+          isMonitorized: document.payload.data().isMonitorized,
+          dateCreated: document.payload.data().dateCreated,
+          creator: document.payload.data().creator,
         };
       }));
+  }
+
+  getAllMonitoredDevices(): Observable<Device[]> {
+    return this.database.collection<Device>(`datos`,
+      ref => ref.where('isMonitorized', '==', true))
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((document) => {
+            return {
+              id: document.payload.doc.id,
+              name: document.payload.doc.data().name,
+              location: document.payload.doc.data().location,
+              status: document.payload.doc.data().status,
+              temperature: document.payload.doc.data().temperature,
+              humidity: document.payload.doc.data().humidity,
+              isMonitorized: document.payload.doc.data().isMonitorized,
+              dateCreated: document.payload.doc.data().dateCreated,
+              creator: document.payload.doc.data().creator,
+            };
+          });
+        }));
+  }
+
+  getAllDevices(): Observable<Device[]> {
+    return this.database.collection<Device>(`datos`)
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((document) => {
+            return {
+              id: document.payload.doc.id,
+              name: document.payload.doc.data().name,
+              location: document.payload.doc.data().location,
+              status: document.payload.doc.data().status,
+              temperature: document.payload.doc.data().temperature,
+              humidity: document.payload.doc.data().humidity,
+              isMonitorized: document.payload.doc.data().isMonitorized,
+              dateCreated: document.payload.doc.data().dateCreated,
+              creator: document.payload.doc.data().creator,
+            };
+          });
+        }));
   }
 }
