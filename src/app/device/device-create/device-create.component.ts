4aaabff441fc56a0693cace151c642226c10741c
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-26 14:35:46 -0500

    Add database writting functionality.

--- src/app/device/device-create/device-create.component.ts
+++ src/app/device/device-create/device-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceService } from '../../device.service';
+import { NgForm } from '@angular/forms';
+import { Device } from '../../device.model';
+import * as moment from 'moment';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-device-create',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-create.component.scss']
 })
 export class DeviceCreateComponent implements OnInit {
+  newId: string;
+  wasCreated = false;
 
-  constructor() { }
+  constructor(private deviceService: DeviceService,
+  public snackbar: MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  onCreateDevice(form: NgForm) {
+    const device: Device = {
+      name: form.value.name,
+      location: form.value.location,
+      status: form.value.status,
+      isMonitorized: form.value.isMonitorized,
+      creator: 'Freddy Chan',
+      dateCreated: moment (Date.now()).valueOf(),
+    };
+
+    console.log(device);
+    // this.id = this.deviceService.createDevice(device);
+    this.newId = '12345678';
+    this.wasCreated = true;
+
+    this.snackbar.open('Dispositivo creado...', null, {
+      duration: 3000
+    });
   }
 
 }
