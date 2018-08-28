23b3917620fc61eb8a9edfd1812389ec53bcbaeb
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-28 00:54:13 -0500

    Modules configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DeviceService } from './device.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  value1 = 50;
+  value2 = 33;
+  value3 = 5;
+  value4 = 88;
+
+  subscriptions: Subscription[] = [];
+
+  constructor(private deviceService: DeviceService) {
+
+  }
+
+  ngOnInit() {
+    // this.subscriptions.push(this.deviceService.getDeviceById('0e2270FEA1g0NEv2bZzT')
+    // .subscribe((datos) => {
+    //   console.log(datos);
+    // }));
+  }
+
+  ngOnDestroy() {
+    // if (this.subscriptions.length > 0) {
+    //   this.subscriptions.forEach(element => {
+    //     element.unsubscribe();
+    //   });
+    // }
+  }
 }
+
+// https://firestore.googleapis.com/v1beta1/projects/sensor-monitor-5e35f/databases/(default)/documents/datos/0e2270FEA1g0NEv2bZzT
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,133 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatDatepickerModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSlideToggleModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatRadioModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatNativeDateModule,
+  MatTooltipModule,
+  MatExpansionModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatProgressBarModule,
+  MatAutocompleteModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
+  MatBadgeModule
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 import { AppComponent } from './app.component';
+import { DeviceService } from './device.service';
+import { ContainerComponent } from './navigation/container/container.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { DeviceListComponent } from './device/device-list/device-list.component';
+import { DeviceHistoryComponent } from './device/device-history/device-history.component';
+import { DeviceConfigComponent } from './device/device-config/device-config.component';
+import { DeviceCreateComponent } from './device/device-create/device-create.component';
+import { DeviceEditComponent } from './device/device-edit/device-edit.component';
+import { DeviceMonitorComponent } from './device/device-monitor/device-monitor.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'device-list', component: DeviceListComponent },
+  { path: 'device-create', component: DeviceCreateComponent },
+  { path: 'device/:id', component: DeviceEditComponent },
+  { path: 'device-monitor', component: DeviceMonitorComponent },
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    LoginComponent,
+    DeviceListComponent,
+    DeviceHistoryComponent,
+    DeviceConfigComponent,
+    DeviceCreateComponent,
+    DeviceEditComponent,
+    DeviceMonitorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatRadioModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatBadgeModule,
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [
+    DeviceService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
