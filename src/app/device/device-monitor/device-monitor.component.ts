e92363cd8172d58851ca751a315f14f3e664054d
Freddy Chan Narváez <freddychan@rammalvarez.com>
2018-08-27 22:23:39 -0500

    Add functionality to monitor.

--- src/app/device/device-monitor/device-monitor.component.ts
+++ src/app/device/device-monitor/device-monitor.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DeviceService } from '../../device.service';
+import { Subscription } from 'rxjs';
+import { Device } from '../../device.model';
 
 @Component({
   selector: 'app-device-monitor',
   templateUrl: './device-monitor.component.html',
   styleUrls: ['./device-monitor.component.scss']
 })
-export class DeviceMonitorComponent implements OnInit {
+export class DeviceMonitorComponent implements OnInit, OnDestroy {
+  isLoaded = false;
+  subscriptions: Subscription[] = [];
+  allDevices: Device[] = [];
 
-  constructor() { }
+  constructor(private deviceService: DeviceService) { }
 
   ngOnInit() {
+    this.subscriptions.push(this.deviceService.getAllMonitoredDevices()
+    .subscribe((devices: Device[]) => {
+      this.allDevices = devices;
+      this.isLoaded = true;
+      console.log(this.allDevices);
+    }));
+  }
+
+  ngOnDestroy() {
+    if (this.subscriptions.length > 0) {
+      this.subscriptions.forEach((subscription: Subscription) => {
+        subscription.unsubscribe();
+      });
+    }
   }
 
 }
